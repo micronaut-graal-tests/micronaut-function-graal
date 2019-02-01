26f7f971d03921c6b27b989d57255a095c4ab58c
Iván López <lopez.ivan@gmail.com>
2019-02-01 12:01:51 +0100

    Add function that receives and returns a POJO

---
+++ src/main/java/function/graal/pojo/GreetingRequest.java
@@ -0,0 +1,21 @@
+package function.graal.pojo;
+
+public class GreetingRequest {
+
+    private String name;
+
+    public GreetingRequest() {
+    }
+
+    public GreetingRequest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/function/graal/pojo/GreetingResponse.java
@@ -0,0 +1,21 @@
+package function.graal.pojo;
+
+public class GreetingResponse {
+
+    private String msg;
+
+    public GreetingResponse() {
+    }
+
+    public GreetingResponse(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
