3ce229095adf2dde682adae8e228ea62a6405e0d
Iván López <lopez.ivan@gmail.com>
2019-09-09 12:19:57 +0200

    Use the right jar in native-image command

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/function-graal-*.jar
+native-image --no-server --no-fallback --class-path build/libs/function-graal-*-all.jar
